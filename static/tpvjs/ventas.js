476a05087cce4a544ceea3b82f1c59c42f1ecf02
Marco Mansilla <marco@saltalug.org.ar>
2013-07-31 16:56:57 -0300

    mejorando la interfaz de ventas

--- static/tpvjs/ventas.js
+++ static/tpvjs/ventas.js
@@ -3,30 +3,28 @@ $(document).ready(function(){
     $(".agregar").click(function() {
 	var q = $(".q").val();
 	$.ajax({ // ajax call starts
-	    url: '/tpv/operaciones/listado.json', // JQuery loads serverside.php
+	    url: '/tpv/operaciones/listado.json', // JQuery loads query generated json
 	    data: 'q=' + $(".q").val(), // Send value of the clicked button
 	    dataType: 'json', // Choosing a JSON datatype
 	    success: function(data) // Variable data contains the data we get from serverside
 	    {
-		for (var i in data.listado) {
-		 
+		for (var i in data.listado) { // this must be a while loop instead with a flag
 		    if (data.listado[i].id == q)
 		    {
-			
 			var tr=$('<tr></tr>');
-		        $('<td>'+data.listado[i].codigo+'</td>').appendTo(tr);
+			$('<td></td>').appendTo(tr);
 			$('<td>'+data.listado[i].nombre+'</td>').appendTo(tr);
-			$('<td>'+data.listado[i].categoria+'</td>').appendTo(tr);
 			$('<td>'+data.listado[i].precio+'</td>').appendTo(tr);
+			$('<td></td>').appendTo(tr);
 		        tr.appendTo('.table');
 			tr.addClass(data.listado[i].id)
-			//$('.articulos').append('articulo:  ' + data.listado[i].nombre + '<br/>');
 		    }
 		}
 	    }
 	});
 	$(".q").val("");
-	$(".q").focus()
+	$(".q").focus();
+	$(".total").text('3434,34');
 	return false; // keeps the page from not refreshing 
 	
     });
